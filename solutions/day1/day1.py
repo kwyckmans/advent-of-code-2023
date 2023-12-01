95e3e44c2d9f146ae08497d116424cd21c64ec2b
kwyckmans <451289+kwyckmans@users.noreply.github.com>
2023-12-01 12:47:55 +0100

    Finish day one

--- solutions/day1/day1.py
+++ solutions/day1/day1.py
@@ -1,37 +1,51 @@
-import heapq
-import math
 from pathlib import Path
 from aoc_utils import loader
 
+digits = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9
+}
+
+def find_calibration_values(s: str) -> int:
+    digits = [c for c in s if c.isdigit()]
+    return int(digits[0] + digits[-1])
+
+def find_calibration_values_with_text(s: str) -> int:
+    first_idx = 10000
+    last_idx = -1
+    for i, c in enumerate(s):
+        if c.isdigit():
+            first_idx = min(first_idx, i)
+            last_idx = max(last_idx, i)
+
+    lowest_digit = s[first_idx] if first_idx < 10000 else 10000
+    highest_digit = s[last_idx] if last_idx >= 0 else -1
+
+    for key, val in digits.items():
+        idx = s.find(key)
+        ridx = s.rfind(key)
+        if idx > -1 and idx < first_idx:
+            first_idx = idx
+            lowest_digit = val
+        
+        if ridx != -1 and ridx  > last_idx:
+            last_idx = ridx 
+            highest_digit = val
+
+    return int(str(lowest_digit) + str(highest_digit))
+
 if __name__ == "__main__":
     p = Path(__file__).parent / "input.txt"
-    
-    running_total = 0
-    max_cals = -math.inf
-    for line in loader.load_per_line(p):
-        if line == "":
-            max_cals = max(max_cals, running_total)
-            running_total = 0
-        else:
-            running_total += int(line)
+    result = sum(list(map(find_calibration_values ,loader.load_per_line(p))))
+    print(result)
 
-    print(max_cals)
+    result = sum(list(map(find_calibration_values_with_text, loader.load_per_line(p))))
+    print(result)
 
-    calories = []
-    heapq.heapify(calories)
-    total = 0
-
-    for line in loader.load_per_line(p):
-        if line == "":
-            heapq.heappush(calories, total)
-            if len(calories) > 3:
-                heapq.heappop(calories)
-            total = 0
-        else:
-            total += int(line)
-
-    print(sum(calories))
-    # calories.sort(reverse=True)
-    # print(sum(calories[:3]))
-
-    
