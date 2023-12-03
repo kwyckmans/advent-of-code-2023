c486fe1e386e0f37551d2c2c6554726360ccc3ac
kwyckmans <451289+kwyckmans@users.noreply.github.com>
2023-12-03 13:23:10 +0100

    Formatting

--- solutions/day3/day3.py
+++ solutions/day3/day3.py
@@ -1,9 +1,17 @@
 from dataclasses import dataclass
 from pathlib import Path
+from typing import List
 from aoc_utils import loader
 
+def solve_part_1(grid: List[List[chr]]) -> int:
+    for row in grid:
+        for char in row:
+            if char.is_digit():
+                print(char)
+    
 
 if __name__ == "__main__":
     p = Path(__file__).parent / "input.txt"
+    grid = loader.load_grid(p)
 
-    print(loader.load_grid(p))
+
